4de263846c4fdc5812a8121e71cc586d7d4a2154
ASPR175 <atharvarisbud2006@gmail.com>
2026-02-21 19:10:04 +0530

    Worker structure defined

--- internal/queue/worker.go
+++ internal/queue/worker.go
@@ -1,2 +1,49 @@
 package queue
 
+
+import (
+	"context"
+	"log"
+	"time"
+)
+
+type JobHandler func(ctx context.Context, job *Job) error
+
+type Worker struct {
+	WorkerID      string
+	Queue   Queue
+	Handler JobHandler
+
+	PollInterval time.Duration
+}
+
+func (w *Worker) Run(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			log.Printf("worker %s shutting down", w.WorkerID)
+			return
+		default:
+		}
+
+		job, err := w.Queue.Lease(ctx, w.WorkerID)
+		if err != nil {
+			if err == ErrNoJob {
+				time.Sleep(w.PollInterval)
+				continue
+			}
+
+			log.Printf("worker %s lease error: %v", w.WorkerID, err)
+			time.Sleep(w.PollInterval)
+			continue
+		}
+
+		err = w.Handler(ctx, job)
+		if err != nil {
+			_ = w.Queue.Nack(ctx, job.JobID, err)
+			continue
+		}
+
+		_ = w.Queue.Ack(ctx, job.JobID)
+	}
+}
